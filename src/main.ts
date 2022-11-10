5899aea07220cd40cbb8cc329a9a965f8a58ced1
Jeszpy <hleb.lukashonak@gmail.com>
2022-11-10 20:10:08 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
 
   const config = new DocumentBuilder()
     .setTitle('Homework-API')
