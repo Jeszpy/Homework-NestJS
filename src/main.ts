fdc0431c227527583ef0934a617e2cb0af78c56f
Jeszpy <hleb.lukashonak@gmail.com>
2022-11-10 19:33:22 +0300

    add nestjs

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Homework-API')
+    .setDescription('IT-Incubator homeworks')
+    .setVersion('1.0')
+    // .addTag('Homework-API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const configService = app.get(ConfigService)
+  const port = configService.get<number>('PORT')
+  
+  await app.listen(port, () => {
+    console.log(`App successfully started at ${port} port`);
+    
+  });
+}
+bootstrap();
