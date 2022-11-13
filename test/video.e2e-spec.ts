c54bda048596d69d4587726173570588bdb32630
Jeszpy <hleb.lukashonak@gmail.com>
2022-11-13 12:47:26 +0300

    complete fist homework and e2e tests

--- test/video.e2e-spec.ts
+++ test/video.e2e-spec.ts
@@ -5,7 +5,7 @@ import { AppModule } from '../src/app.module';
 import { endpoints } from './helpers/routing';
 import { wipeAllData } from './helpers/general-functions';
 import { createApp } from '../src/helpers/create-app';
-import { subDays } from 'date-fns';
+import { addDays, subDays } from 'date-fns';
 
 describe('Video Controller', () => {
   let app: INestApplication;
@@ -16,11 +16,17 @@ describe('Video Controller', () => {
       title: 'valid title',
       author: 'valid author',
       availableResolutions: ['P144', 'P360'],
+      canBeDownloaded: true,
+      minAgeRestriction: 18,
+      publicationDate: addDays(new Date(), 5).toISOString(),
     },
     invalid: {
       title: '',
       author: '',
       availableResolutions: ['P145'],
+      canBeDownloaded: null,
+      minAgeRestriction: 22,
+      publicationDate: null,
     },
   };
 
@@ -55,7 +61,15 @@ describe('Video Controller', () => {
     });
   });
 
-  describe('Create Video /videos (POST)', () => {
+  describe('Create video /videos (POST)', () => {
+    it('should return 404 status code', async () => {
+      const response = await request(server)
+        .put(`${endpoints.videoController}/-1`)
+        .send(preparedData.valid);
+
+      expect(response).toBeDefined();
+      expect(response.status).toBe(404);
+    });
     it('should return 204 and array of errors', async () => {
       const response = await request(server)
         .post(endpoints.videoController)
@@ -95,17 +109,157 @@ describe('Video Controller', () => {
         subDays(new Date(video.publicationDate), 1),
       );
       expect(createdAt).toBe(publicationDate);
+
+      const getVideoById = await request(server).get(
+        `${endpoints.videoController}/${video.id}`,
+      );
+
+      expect(getVideoById).toBeDefined();
+      expect(getVideoById.status).toBe(200);
+      expect(getVideoById.body).toEqual(video);
+
       expect.setState({ video });
+    });
+  });
+  describe('Get all videos /videos (GET)', () => {
+    it('should return one video', async () => {
+      const firstVideo = expect.getState().video;
 
-      //TODO: сделать геты, апдэйты, делиты
+      const getAllVideos = await request(server).get(
+        `${endpoints.videoController}/`,
+      );
 
-      // const response = await request(server).get(
-      //   `${endpoints.videoController}/${video.id}`,
-      // );
-      //
-      // expect(response).toBeDefined();
-      // expect(response.status).toBe(200);
-      // expect(response.body).toEqual(video);
+      expect(getAllVideos).toBeDefined();
+      expect(getAllVideos.status).toBe(200);
+      expect(getAllVideos.body).toEqual([firstVideo]);
+    });
+    it('should return 5 videos, addition methods: /videos (POST)', async () => {
+      for (let i = 0; i < 4; i++) {
+        const createVideo = await request(server)
+          .post(endpoints.videoController)
+          .send(preparedData.valid);
+
+        expect(createVideo).toBeDefined();
+        expect(createVideo.status).toBe(201);
+      }
+
+      const getAllVideos = await request(server).get(endpoints.videoController);
+
+      expect(getAllVideos).toBeDefined();
+      expect(getAllVideos.status).toBe(200);
+      expect(getAllVideos.body.length).toBe(5);
+      expect(getAllVideos.body).toEqual(expect.any(Array));
+    });
+  });
+
+  describe('Update one video by id /videos (POST)', () => {
+    it('should return 404 status code', async () => {
+      const response = await request(server)
+        .put(`${endpoints.videoController}/-1`)
+        .send(preparedData.valid);
+
+      expect(response).toBeDefined();
+      expect(response.status).toBe(404);
+    });
+    it('should return array of errors and 400 status code', async () => {
+      const video = expect.getState().video;
+      const response = await request(server)
+        .put(`${endpoints.videoController}/${video.id}`)
+        .send(preparedData.invalid);
+
+      expect(response).toBeDefined();
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({
+        errorsMessages: [
+          { message: expect.any(String), field: 'canBeDownloaded' },
+          { message: expect.any(String), field: 'minAgeRestriction' },
+          { message: expect.any(String), field: 'publicationDate' },
+          { message: expect.any(String), field: 'title' },
+          { message: expect.any(String), field: 'author' },
+          { message: expect.any(String), field: 'availableResolutions' },
+        ],
+      });
+    });
+    it('should update video and return 204 status code. Use Additional methods: /videos/:id (GET)', async () => {
+      const video = expect.getState().video;
+      const response = await request(server)
+        .put(`${endpoints.videoController}/${video.id}`)
+        .send(preparedData.valid);
+
+      expect(response).toBeDefined();
+      expect(response.status).toBe(204);
+
+      const getVideoById = await request(server).get(
+        `${endpoints.videoController}/${video.id}`,
+      );
+
+      expect(getVideoById).toBeDefined();
+      expect(getVideoById.status).toBe(200);
+      expect(getVideoById.body).not.toEqual(video);
+      expect(getVideoById.body).toStrictEqual({
+        id: video.id,
+        title: preparedData.valid.title,
+        author: preparedData.valid.author,
+        minAgeRestriction: preparedData.valid.minAgeRestriction,
+        canBeDownloaded: preparedData.valid.canBeDownloaded,
+        createdAt: expect.any(String),
+        publicationDate: preparedData.valid.publicationDate,
+        availableResolutions: preparedData.valid.availableResolutions,
+      });
+    });
+  });
+
+  describe('Delete video by id and then delete all videos', () => {
+    it('should return 404 status code', async () => {
+      const response = await request(server).delete(
+        `${endpoints.videoController}/-1`,
+      );
+
+      expect(response).toBeDefined();
+      expect(response.status).toBe(404);
+    });
+    it('should return 204 status code. Use Additional methods: /videos/:id (GET)', async () => {
+      const video = expect.getState().video;
+      const response = await request(server).delete(
+        `${endpoints.videoController}/${video.id}`,
+      );
+
+      expect(response).toBeDefined();
+      expect(response.status).toBe(204);
+      expect(response.body).toEqual({});
+
+      const getDeletedVideo = await request(server).get(
+        `${endpoints.videoController}/${video.id}`,
+      );
+      expect(getDeletedVideo).toBeDefined();
+      expect(getDeletedVideo.status).toBe(404);
+      expect(getDeletedVideo.body).toEqual({});
+    });
+    it('should return 204 status code. Use Additional methods: /videos (GET)', async () => {
+      const getAllVideosAfterDelete = await request(server).get(
+        `${endpoints.videoController}/`,
+      );
+      expect(getAllVideosAfterDelete).toBeDefined();
+      expect(getAllVideosAfterDelete.status).toBe(200);
+      expect(getAllVideosAfterDelete.body.length).toBe(4);
+
+      const videosId = getAllVideosAfterDelete.body.map((v) => v.id);
+      for (const id of videosId) {
+        const response = await request(server).delete(
+          `${endpoints.videoController}/${+id}`,
+        );
+
+        expect(response).toBeDefined();
+        expect(response.status).toBe(204);
+        expect(response.body).toEqual({});
+      }
+
+      const getAllVideosBeforeDelete = await request(server).get(
+        `${endpoints.videoController}/`,
+      );
+      expect(getAllVideosBeforeDelete).toBeDefined();
+      expect(getAllVideosBeforeDelete.status).toBe(200);
+      expect(getAllVideosBeforeDelete.body.length).toBe(0);
     });
   });
 });
