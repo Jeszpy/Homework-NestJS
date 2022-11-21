fecf8f30ee0893c6ba29565083f2ea67e49e2fcb
Jeszpy <hleb.lukashonak@gmail.com>
2022-11-21 23:51:25 +0300

    TestingRepository => add postModel for delete

--- src/modules/testing/infrastructure/testing.repository.mongodb.ts
+++ src/modules/testing/infrastructure/testing.repository.mongodb.ts
@@ -3,6 +3,7 @@ import { Video, VideoDocument } from '../../video/models/video.schema';
 import mongoose, { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { Blog, BlogDocument } from '../../blog/models/blogs.schema';
+import { Post, PostDocument } from '../../post/models/post.schema';
 
 @Injectable()
 export class TestingRepository {
@@ -10,12 +11,14 @@ export class TestingRepository {
     @InjectModel(Video.name)
     private readonly videoModel: mongoose.Model<VideoDocument>,
     @InjectModel(Blog.name) private readonly blogModel: Model<BlogDocument>,
+    @InjectModel(Post.name) private readonly postModel: Model<PostDocument>,
   ) {}
 
   async wipeAllData(): Promise<boolean> {
     try {
       await this.videoModel.deleteMany({});
       await this.blogModel.deleteMany({});
+      await this.postModel.deleteMany({});
       return true;
     } catch (e) {
       return false;
--- src/modules/testing/testing.module.ts
+++ src/modules/testing/testing.module.ts
@@ -5,10 +5,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Video, VideoSchema } from '../video/models/video.schema';
 import { TestingRepository } from './infrastructure/testing.repository.mongodb';
 import { Blog, BlogSchema } from '../blog/models/blogs.schema';
+import { Post, PostSchema } from '../post/models/post.schema';
 
 const schemas = [
   { name: Video.name, schema: VideoSchema },
   { name: Blog.name, schema: BlogSchema },
+  { name: Post.name, schema: PostSchema },
 ];
 
 @Module({
