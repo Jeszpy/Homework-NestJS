bd5b1450f7da367e37cd38df58d0c82d374696a1
Jeszpy <hleb.lukashonak@gmail.com>
2022-12-17 19:00:35 +0300

    remove refreshToken blackList, add logic in find device

--- src/modules/security/api/security.controller.ts
+++ src/modules/security/api/security.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get, HttpCode, UseGuards } from '@nestjs/common';
-import { SessionQueryRepositoryMongodb } from '../session/infrastructure/session-query.repository.mongodb';
-import { RefreshTokenGuard } from '../../guards/refresh-token.guard';
-import { User } from '../../decorators/param/user.decorator';
-import { UserEntity } from '../user/models/user.schema';
-import { SessionViewModel } from '../session/models/session-view.model';
+import { SessionQueryRepositoryMongodb } from '../../session/infrastructure/session-query.repository.mongodb';
+import { RefreshTokenGuard } from '../../../guards/refresh-token.guard';
+import { User } from '../../../decorators/param/user.decorator';
+import { UserEntity } from '../../user/models/user.schema';
+import { SessionViewModel } from '../../session/models/session-view.model';
 
 @Controller('security')
 export class SecurityController {
--- src/modules/security/security.module.ts
+++ src/modules/security/security.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { SecurityController } from './security.controller';
+import { SecurityController } from './api/security.controller';
 import { JwtService } from '../auth/application/jwt.service';
 import { UserQueryRepositoryMongodb } from '../user/infrastructure/user-query.repository.mongodb';
 import { SessionQueryRepositoryMongodb } from '../session/infrastructure/session-query.repository.mongodb';
