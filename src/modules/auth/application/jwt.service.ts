47d286ce57d051248ebb4dc2d7b9f1ac2b8b90e8
Jeszpy <hleb.lukashonak@gmail.com>
2022-12-25 03:40:28 +0300

    try to manual rollback

--- src/modules/auth/application/jwt.service.ts
+++ src/modules/auth/application/jwt.service.ts
@@ -8,26 +8,23 @@ import configuration from '../../../config/configuration';
 @Injectable()
 export class JwtService {
   constructor(
-    private readonly configService: ConfigService, // private readonly userQueryRepository: UserQueryRepositoryMongodb,
+      private readonly configService: ConfigService, // private readonly userQueryRepository: UserQueryRepositoryMongodb,
   ) {}
 
+  private accessTokenSecretKey = this.configService.get<string>(
+      'ACCESS_TOKEN_SECRET',
+  );
 
   private accessTokenLifeTime = this.configService.get<string>(
-    'ACCESS_TOKEN_LIFE_TIME',
+      'ACCESS_TOKEN_LIFE_TIME',
   );
 
-  private accessTokenSecretKey = this.configService.get<string>(
-    'ACCESS_TOKEN_SECRET',
-  );
-
-  private refreshTokenLifeTime = this.configService.get<string>(
-    'REFRESH_TOKEN_LIFE_TIME',
-  );
-  
   private refreshTokenSecretKey = this.configService.get<string>(
-    'REFRESH_TOKEN_SECRET',
+      'REFRESH_TOKEN_SECRET',
   );
-  
+  private refreshTokenLifeTime = this.configService.get<string>(
+      'REFRESH_TOKEN_LIFE_TIME',
+  );
 
   async signAccessToken(userId: string, deviceId = '123'): Promise<string> {
     return jwt.sign({ userId, deviceId }, this.accessTokenSecretKey, {
@@ -61,18 +58,18 @@ export class JwtService {
 
   async signAccessAndRefreshTokenToken(userId: string, deviceId: string) {
     const accessToken = jwt.sign(
-      { userId, deviceId },
-      this.accessTokenSecretKey,
-      {
-        expiresIn: this.accessTokenLifeTime,
-      },
+        { userId, deviceId },
+        this.accessTokenSecretKey,
+        {
+          expiresIn: this.accessTokenLifeTime,
+        },
     );
     const refreshToken = jwt.sign(
-      { userId, deviceId },
-      this.refreshTokenSecretKey,
-      {
-        expiresIn: this.refreshTokenLifeTime,
-      },
+        { userId, deviceId },
+        this.refreshTokenSecretKey,
+        {
+          expiresIn: this.refreshTokenLifeTime,
+        },
     );
     return { accessToken, refreshToken };
   }
