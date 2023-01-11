2fe27ede4667f898ce6b6a32804914f567130801
Jeszpy <hleb.lukashonak@gmail.com>
2023-01-11 15:42:41 +0300

    change structure for 15 hw

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { createApp } from './helpers/create-app';
+
 // const fn = (len: number) => {
 //   const str = new Array(len + 1).join('123');
 //   console.log(str.length);
