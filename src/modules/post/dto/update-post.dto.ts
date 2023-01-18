c0e1b873d9fda56d67f08f88df4807363ccf4b68
Jeszpy <hleb.lukashonak@gmail.com>
2023-01-18 13:18:16 +0300

    fix updatePostById and deletePostById

--- src/modules/post/dto/update-post.dto.ts
+++ src/modules/post/dto/update-post.dto.ts
@@ -1,3 +1,20 @@
 import { CreatePostWithBlogIdDto } from './create-post.dto';
+import { IsString, MaxLength } from 'class-validator';
+import { Trim } from '../../../decorators/validation/trim.decorator';
 
-export class UpdatePostDto extends CreatePostWithBlogIdDto {}
+// export class UpdatePostDto extends CreatePostWithBlogIdDto {}
+
+export class UpdatePostDto {
+  @IsString()
+  @Trim()
+  @MaxLength(30)
+  title: string;
+  @IsString()
+  @Trim()
+  @MaxLength(100)
+  shortDescription: string;
+  @IsString()
+  @Trim()
+  @MaxLength(1000)
+  content: string;
+}
